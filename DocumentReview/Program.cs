65d89e7f753f82ce460d3486de91cdc620604310
AndreyGolubkow <AndreyGolubkow@gmail.com>
2019-11-02 05:55:22 +0700

    initial

---
+++ DocumentReview/Program.cs
@@ -0,0 +1,21 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Linq;
+
+namespace DocumentReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var doc = WordprocessingDocument.Open(@"D:\test.docx", false))
+            {
+                foreach (var el in doc.MainDocumentPart.Document.Body.Elements().OfType<Paragraph>())
+                {
+                    Console.WriteLine(el.InnerText);
+                }
+            }
+        }
+    }
+}
